9dc442cfa2fb9d335c12c53799fe39f885acd567
JoseRobertoPortes <portes.git@gmail.com>
2026-08-17 17:34:50 -0300

    TaskTrackerCLI.v2

--- __main__.py
+++ __main__.py
@@ -1,22 +1,35 @@
 from classes.classes import *
 import sys
-import json
-
 
 def main():
-
-    if sys.argv[1] == "view":
-        apresentar_lista()
+    if sys.argv[1] == "list":
+        all_tasks()
     elif sys.argv[1] == "add":
-        add_Tarefa(sys.argv[2], sys.argv[3])
+        add(sys.argv[2], sys.argv[3])
 
-    elif sys.argv[1] == "priority":
-        select_status(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == "upnome":
+        update_nome(sys.argv[2], sys.argv[3])
+
+    elif sys.argv[1] == "uppriority":
+        update_prioridade(sys.argv[2], sys.argv[3])
+
+    elif sys.argv[1] == "upstatus":
+        update_status(sys.argv[2], sys.argv[3])
 
     elif sys.argv[1] == "del":
-        del_tarefa(sys.argv[2])
+            del_task(sys.argv[2])
 
 
-if __name__ == "__main__":
+    elif sys.argv[1] == "undone":
+            undone_tasks()
+
+
+    elif sys.argv[1] == "done":
+            done_tasks()
+
+    elif sys.argv[1] == "inprogress":
+            inprogress_tasks()
+
+
+if __name__ == '__main__':
     main()
-
--- classes/classes.py
+++ classes/classes.py
@@ -1,51 +1,98 @@
 import json
 
+
+
 def carregar_tarefas():
-    with open('lista_tarefas.json', 'r', encoding='utf-8') as arquivo:
-        return json.load(arquivo, ensure_ascii=False)
+    try:
+        with open('lista_tarefas.json', 'r') as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        lista_vazia = []
+        with open('lista_tarefas.json', 'w') as arquivo:
+            json.dump(lista_vazia, arquivo)
+    return lista_vazia
+
 
 lista_tarefas = carregar_tarefas()
 
 
-def add_Tarefa(tarefa, prioridade):
-    novo_id = lista_tarefas[-1]["Id"] + 1 if lista_tarefas else 1
-    tarefa_atual = {
-        "Id": novo_id,
-        "Nome": tarefa,
-        "Prioridade": prioridade,
-        "Status": "To_do"
+def add(nome, prioridade):
+    novo_id = lista_tarefas[-1]['id'] + 1 if lista_tarefas else 1
+    nova_tarefa = {
+        'id': novo_id,
+        'nome': nome,
+        'prioridade': prioridade,
+        'status': 'to do'
     }
-    lista_tarefas.append(tarefa_atual)
-    with open('lista_tarefas.json', "w", encoding='utf-8') as arquivo:
-        json.dump(lista_tarefas,arquivo, ensure_ascii=False)
-    return tarefa
+    lista_tarefas.append(nova_tarefa)
+    with open('lista_tarefas.json', 'w') as arquivo:
+        json.dump(lista_tarefas, arquivo)
 
 
-def apresentar_lista():
-    for c in range(len(lista_tarefas)):
-        print(lista_tarefas[c])
-
-
-def select_status(id_tarefa, status):
-    conversor = int(id_tarefa)
+def update_nome(id, novo_nome):
+    idint = int(id)
     for tarefa in lista_tarefas:
-        if tarefa["Id"] == conversor:
-            tarefa["Status"] = status
-            with open('lista_tarefas.json', "w", encoding='utf-8') as arquivo:
-                json.dump(lista_tarefas, arquivo, ensure_ascii=False)
+        if tarefa['id'] == idint:
+            tarefa['nome'] = novo_nome
+            with open('lista_tarefas.json', 'w') as arquivo:
+                json.dump(lista_tarefas, arquivo)
             return True
-    return False
 
-def del_tarefa(id_tarefa):
-    conversor = int(id_tarefa)
+
+
+def update_prioridade(id, nova_prioridade):
+    idint = int(id)
     for tarefa in lista_tarefas:
+        if tarefa['id'] == idint:
+            tarefa['prioridade'] = nova_prioridade
+            with open('lista_tarefas.json', 'w') as arquivo:
+                json.dump(lista_tarefas, arquivo)
+            return True
 
-        if tarefa["Id"] == conversor:
+
+def update_status(id, novo_status):
+    idint = int(id)
+    for tarefa in lista_tarefas:
+        if tarefa['id'] == idint:
+            tarefa['status'] = novo_status
+            with open('lista_tarefas.json', 'w') as arquivo:
+                json.dump(lista_tarefas, arquivo)
+            return True
+
+def del_task(id):
+    idint = int(id)
+    for tarefa in lista_tarefas:
+        if tarefa['id'] == idint:
             lista_tarefas.remove(tarefa)
-            with open('lista_tarefas.json', 'w', encoding='utf-8') as arquivo:
-                json.dump(lista_tarefas,arquivo, ensure_ascii=False)
-
+            with open('lista_tarefas.json', 'w') as arquivo:
+                json.dump(lista_tarefas, arquivo)
             return True
-    return False
 
 
+
+def all_tasks():
+    for tarefa in lista_tarefas:
+        print(
+            f'Id: {tarefa['id']}, Nome: {tarefa['nome']}, Prioridade: {tarefa['prioridade']}, Status: {tarefa['status']}')
+
+
+def done_tasks():
+    for tarefa in lista_tarefas:
+        if tarefa['status'] == 'done':
+            print(
+                f'Id: {tarefa['id']}, Nome: {tarefa['nome']}, Prioridade: {tarefa['prioridade']}, Status: {tarefa['status']}')
+
+
+
+def undone_tasks():
+    for tarefa in lista_tarefas:
+        if tarefa['status'] != 'done':
+            print(
+                f'Id: {tarefa['id']}, Nome: {tarefa['nome']}, Prioridade: {tarefa['prioridade']}, Status: {tarefa['status']}')
+
+
+def inprogress_tasks():
+    for tarefa in lista_tarefas:
+        if tarefa['status'] == 'in progress':
+            print(
+                f'Id: {tarefa['id']}, Nome: {tarefa['nome']}, Prioridade: {tarefa['prioridade']}, Status: {tarefa['status']}')
